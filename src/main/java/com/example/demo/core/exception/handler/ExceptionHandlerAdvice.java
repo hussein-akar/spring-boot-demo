b97da6ab40022273c3508b55a4a90f28185de487
Hussein Akar <hak@container-xchange.com>
2023-04-15 17:02:56 +0200

    feat(Config): add global exception handler

---
+++ src/main/java/com/example/demo/core/exception/handler/ExceptionHandlerAdvice.java
@@ -0,0 +1,22 @@
+package com.example.demo.core.exception.handler;
+
+import com.example.demo.core.exception.ForbiddenException;
+import com.example.demo.core.exception.UseCaseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(UseCaseException.class)
+    public ResponseEntity<Object> handleShowMessageToUserException(UseCaseException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<Object> handleForbiddenException(ForbiddenException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    }
+}
